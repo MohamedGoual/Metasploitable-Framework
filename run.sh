7814e2a07ea44200d7c0c8c0ea0cdde533a98749
MohamedGoual <services.mgos@gmail.com>
2025-09-03 12:58:35 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,31 +1,68 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
+
+center_banner() {
+    local termwidth=$(stty size | cut -d" " -f2)
+    local banner=(
+        " __  __      _                  _       _ _        _     _      "
+        "|  \/  | ___| |_ __ _ ___ _ __ | | ___ (_) |_ __ _| |__ | | ___ "
+        "| |\/| |/ _ \ __/ _` / __| '_ \| |/ _ \| | __/ _` | '_ \| |/ _ \\"
+        "| |  | |  __/ || (_| \__ \ |_) | | (_) | | || (_| | |_) | |  __/"
+        "|_|  |_|\___|\__\__,_|___/ .__/|_|\___/|_|\__\__,_|_.__/|_|\___|"
+        "                         |_|                                     "
+    )
+    echo -e "\e[34m"
+    for line in "${banner[@]}"; do
+        printf "%*s\n" $(( (termwidth + ${#line}) / 2 )) "$line"
+    done
+    echo -e "\e[0m"
+}
+
+center() {
+  termwidth=$(stty size | cut -d" " -f2)
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
 
 clear
-echo -e "\e[32m"
-echo "======================================================="
-echo "           Installing Metasploit on Termux            "
-echo "======================================================="
-echo -e "\e[0m"
+center_banner
 
-pkg update -y && pkg upgrade -y
-pkg install -y wget git curl ruby python3 clang libpcap-dev libxml2 libxslt-dev postgresql postgresql-dev termux-tools make gawk zip unzip
+center "Loading..."
+source <(echo "c3Bpbm5lcj0oICd8JyAnLycgJy0nICdcJyApOwoKY291bnQoKXsKICBzcGluICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9uZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBpIGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcciRpIjsKICAgICAgc2xlZXAgMC4yOwogICAgZG9uZTsKICBkb25lCn0KCmNvdW50" | base64 -d)
 
-git clone https://github.com/rapid7/metasploit-framework.git /data/data/com.termux/files/home/metasploit-framework
+center "* Dependencies installation..."
+pkg update -y
+pkg upgrade -y -o Dpkg::Options::="--force-confnew"
+pkg install -y binutils python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
+python3 -m pip install requests
 
-cd /data/data/com.termux/files/home/metasploit-framework
+center "* Erasing old metasploit folder..."
+if [ -d "${PREFIX}/opt/metasploit-framework" ]; then
+  rm -rf ${PREFIX}/opt/metasploit-framework
+fi
+
+center "* Downloading..."
+if [ ! -d "${PREFIX}/opt" ]; then
+  mkdir ${PREFIX}/opt
+fi
+git clone https://github.com/rapid7/metasploit-framework.git --depth=1 ${PREFIX}/opt/metasploit-framework
+
+center "* Installation..."
+cd ${PREFIX}/opt/metasploit-framework
 gem install bundler
+NOKOGIRI_VERSION=$(cat Gemfile.lock | grep -i nokogiri | sed 's/nokogiri [\(\)]/(/g' | cut -d ' ' -f 5 | grep -oP "(.).[[:digit:]][\w+]?[.].")
+gem install nokogiri -v $NOKOGIRI_VERSION -- --with-cflags="-Wno-implicit-function-declaration -Wno-deprecated-declarations -Wno-incompatible-function-pointer-types" --use-system-libraries
 bundle install
+gem install actionpack
+bundle update activesupport
+bundle update --bundler
+bundle install -j$(nproc --all)
 
-pg_ctl start -D $PREFIX/var/lib/postgresql
-createuser msfuser -P
-createdb msfdb -O msfuser
+ln -sf ${PREFIX}/opt/metasploit-framework/msfconsole ${PREFIX}/bin/
+ln -sf ${PREFIX}/opt/metasploit-framework/msfvenom ${PREFIX}/bin/
+ln -sf ${PREFIX}/opt/metasploit-framework/msfrpcd ${PREFIX}/bin/
+termux-elf-cleaner ${PREFIX}/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
 
-ln -sf /data/data/com.termux/files/home/metasploit-framework/msfconsole $PREFIX/bin/msfconsole
-ln -sf /data/data/com.termux/files/home/metasploit-framework/msfvenom $PREFIX/bin/msfvenom
-
-chmod +x /data/data/com.termux/files/home/metasploit-framework/msfconsole
-
-echo -e "\e[32m"
-echo "Metasploit Framework (msfconsole) has been successfully installed!"
-echo -e "\e[0m"
-echo "You can start Metasploit by running 'msfconsole' command."
+echo -e "\033[32m"
+center "Installation complete"
+echo -e "\nStart Metasploit using the command: msfconsole"
+echo -e "\033[0m"
